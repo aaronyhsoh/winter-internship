6caaae447284a9b5011752f1ba15eaae87b3a1b5
ruthchonggg <ruth.cjn@gmail.com>
2022-12-16 17:56:08 +0800

    update gitignore

--- backend/contracts/src/main/java/com/crosschain/states/HtlcStatusEnum.java
+++
@@ -1,7 +0,0 @@
-package com.crosschain.states;
-
-public enum HtlcStatusEnum {
-    SENDER_INITIATED,
-    TOKEN_WITHDRAWED,
-    TRANSACTION_TIMEOUT
-}
---
+++ backend/workflows/src/main/java/com/crosschain/flows/CheckHtlc.java
@@ -0,0 +1,4 @@
+package com.crosschain.flows;
+
+public class CheckHtlc {
+}
