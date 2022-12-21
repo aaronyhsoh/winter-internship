2e8f0644075fa1fac8fd53c7e9038a33791f89c8
kaceycsn <119840829+kaceycsn@users.noreply.github.com>
2022-12-21 17:50:49 +0800

    //

--- frontend/winterfrontend/src/components/Navigation.jsx
+++ frontend/winterfrontend/src/components/Navigation.jsx
@@ -1,17 +1,21 @@
 import { Link } from "react-router-dom";
+import classes from "./Navigation.module.css";
 
 function Navigation(){
     return(
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">All Bonds</Link>
-                </li>
-                <li>
-                    <Link to="/new-bond">Create Bond</Link>
-                </li>
-            </ul>
-        </nav>
+        <header className={classes.header}>
+            <div className={classes.logo}>Bonds</div>        
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">All Bonds</Link>
+                        </li>
+                        <li>
+                            <Link to="/new-bond">Create Bond</Link>
+                        </li>
+                    </ul>
+                </nav>
+        </header>
     );
 }
 
