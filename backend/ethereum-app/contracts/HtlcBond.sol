109a2bb9bf04c1117d4f76628f3d147a7753fd19
ruthchonggg <ruth.cjn@gmail.com>
2022-12-23 11:12:22 +0800

    updated mapping and debug eth htlc contract

--- backend/ethereum-app/contracts/HtlcBond.sol
+++ backend/ethereum-app/contracts/HtlcBond.sol
@@ -6,13 +6,13 @@ contract HtlcBond {
     constructor() public {
     }
 
-    mapping(address => Htlc) senderHtlc;
-    mapping(address => Htlc) receiverHtlc; 
+    mapping(address => address) senderToReceiver;
+    mapping(address => Htlc) receiveHtlc;
 
     struct Htlc {
         address sender;
         address receiver;
-        string hash;
+        bytes32 hash;
         string secret;
         uint256 timeout; // timeout time if secret has not been provided by then
         uint256 price;
@@ -23,12 +23,11 @@ contract HtlcBond {
     event debugSecret(bytes32);
     event generatedHash(bytes32);
 
-    function createHtlc(address receiver, string memory hash, uint256 timeoutPeriod) public payable {
-        require(senderHtlc[msg.sender].sender == address(0) , "Sender have an existing open htlc contract");
+    function createHtlc(address receiver, bytes32 hash, uint256 timeoutPeriod) public payable {
+        require(senderToReceiver[msg.sender] == address(0) , "Sender have an existing htlc contract");
         require(msg.value > 0, "No ether received");
 
-        // create new htlc contract
-        Htlc storage newHtlc = senderHtlc[msg.sender];
+        Htlc storage newHtlc = receiveHtlc[receiver];
         newHtlc.sender = msg.sender;
         newHtlc.receiver = receiver;
         newHtlc.hash = hash;
@@ -36,30 +35,45 @@ contract HtlcBond {
         newHtlc.price = msg.value;
         emit create("Successful");
 
-        senderHtlc[msg.sender] = newHtlc;
-        receiverHtlc[receiver] = newHtlc;
+        receiveHtlc[receiver] = newHtlc;
+        senderToReceiver[msg.sender] = receiver;
     }
 
-    // but how to ensure that the hash in corda and eth is the same
-    function withdrawEther(string memory secret) public {
-        Htlc storage htlc = receiverHtlc[msg.sender];
-
-        emit debugSecret(keccak256(abi.encodePacked(secret)));
-        emit generatedHash(bytes32(bytes(htlc.hash)));
+    // but how to ensure that the hash in corda and eth is the same?
+    // receiver 
+    function withdrawEther(string memory secret) public payable {
+        Htlc storage htlc = receiveHtlc[msg.sender];
         require(htlc.sender != address(0), "No htlc for withdrawal found");
-       
-        require(keccak256(abi.encodePacked(secret)) == bytes32(bytes(htlc.hash)), "Secret provided is incorrect");
+
+        // verify timeout
+        require(block.timestamp < htlc.timeout, "Timeout exceeded, swap is invalid");
+        
+        // verify secret
+        bytes32 encodedSecret = keccak256(abi.encodePacked(secret));
+        bytes32 hash = htlc.hash;
+        require(encodedSecret == hash, "Secret provided is incorrect");
 
         // release ether
         payable (msg.sender).transfer(htlc.price);
 
         // remove from mapping
-        delete receiverHtlc[msg.sender];
-        delete senderHtlc[htlc.sender];     
+        delete receiveHtlc[msg.sender];
+        senderToReceiver[htlc.sender] = address(0);     
+
+        require(receiveHtlc[msg.sender].sender == address(0), "receiver mapping not deleted");
+        require(senderToReceiver[htlc.sender] == address(0), "sender mapping not deleted");
         emit withdraw("Withdrawal successful");
     }
 
     function getContractBalance() public view returns(uint256) {
         return address(this).balance;
+    }
+
+    function pendingPartyToWithdrawl() public view returns(address) {
+        return senderToReceiver[msg.sender];
+    }
+
+    function pendingReceiveFrom() public view returns(address) {
+        return receiveHtlc[msg.sender].sender;
     }
 }
