01d2605caeab0f99e94259055a0eef7bec832649
ruthchonggg <ruth.cjn@gmail.com>
2022-12-29 17:57:01 +0800

    resolve conflict

--- frontend/winterfrontend/src/components/Navigation.jsx
+++ frontend/winterfrontend/src/components/Navigation.jsx
@@ -1,34 +1,34 @@
 import { Link } from "react-router-dom";
 import classes from "./Navigation.module.css";
 
-function Navigation(){
-    return(
+function Navigation() {
+    return (
         <header className={classes.header}>
             <div className={classes.logo}>
                 <Link to="/">Home</Link>
-            </div>        
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/all-bonds">My Bonds</Link>
-                        </li>
-                        <li>
-                            <Link to="/new-bond">Create Bond</Link>
-                        </li>
-                        <li>
-                            <Link to="/transfer-bond">Transfer Bond</Link>
-                        </li>
-                        <li>
-                            <Link to="/withdraw-bond">Withdraw Bond</Link>
-                        </li>
-                        <li>
-                            <Link to="/refund-bond">Refund Bond</Link>
-                        </li>
-                        <li>
-                            <Link to="/bond-htlc">Htlc</Link>
-                        </li>
-                    </ul>
-                </nav>
+            </div>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/all-bonds">My Bonds</Link>
+                    </li>
+                    <li>
+                        <Link to="/new-bond">Create Bond</Link>
+                    </li>
+                    <li>
+                        <Link to="/transfer-bond">Transfer Bond</Link>
+                    </li>
+                    <li>
+                        <Link to="/withdraw-bond">Withdraw Bond</Link>
+                    </li>
+                    <li>
+                        <Link to="/refund-bond">Refund Bond</Link>
+                    </li>
+                    <li>
+                        <Link to="/bond-htlc">Htlc</Link>
+                    </li>
+                </ul>
+            </nav>
         </header>
     );
 }
